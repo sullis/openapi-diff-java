9026f4b612f06195e284dc52af1ec11c07cb5da4
Sean C. Sullivan <github@seansullivan.com>
2021-01-30 15:10:38 -0800

    Google Truth --> AssertJ

--- src/test/java/io/github/sullis/openapi/diff/DiffCalculatorTest.java
+++ src/test/java/io/github/sullis/openapi/diff/DiffCalculatorTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static com.google.common.truth.Truth.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static io.github.sullis.openapi.diff.TestUtil.*;
 
 public class DiffCalculatorTest {
